913d201dcb93cba0f1d6034d54954b74e3dc6b51
Samir Char Iglesias <31636610+samirchar@users.noreply.github.com>
2022-03-23 11:28:15 -0400

    real first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Multi modal deep learning model to predict movie ratings and revenue',
+    author='samir char and Srujan Esanakarra',
+    license='MIT',
+)
