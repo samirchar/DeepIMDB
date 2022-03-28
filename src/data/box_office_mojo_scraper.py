c0be8b60bea437b9eba89974655dfbf69383a7cc
Samir Char Iglesias <31636610+samirchar@users.noreply.github.com>
2022-03-27 23:05:29 -0400

    reformatted with black

--- src/data/box_office_mojo_scraper.py
+++ src/data/box_office_mojo_scraper.py
@@ -7,13 +7,18 @@ import pandas as pd
 from stem.control import Controller
 from stem import Signal
 
+
 def get_tor_session():
     # initialize a requests Session
     session = requests.Session()
-    # setting the proxy of both http & https to the localhost:9050 
+    # setting the proxy of both http & https to the localhost:9050
     # this requires a running Tor service in your machine and listening on port 9050 (by default)
-    session.proxies = {"http": "socks5://localhost:9050", "https": "socks5://localhost:9050"}
+    session.proxies = {
+        "http": "socks5://localhost:9050",
+        "https": "socks5://localhost:9050",
+    }
     return session
+
 
 def renew_connection():
     with Controller.from_port(port=9051) as c:
@@ -23,96 +28,102 @@ def renew_connection():
 
 
 class BoxOfficeMojoScraper:
-    
-    def __init__(self,imdb_id_list,tor_sess=None):
+    def __init__(self, imdb_id_list, tor_sess=None):
         self.imdb_id_list = imdb_id_list
         self.movies = []
         self.tor_sess = tor_sess
-        
-    def __get_movies_objects(self,imdb_id):
-        
+
+    def __get_movies_objects(self, imdb_id):
+
         imdb_id = str(int(imdb_id)).zfill(7)
         url = f"https://www.boxofficemojo.com/title/tt{imdb_id}/"
-        
-        
+
         table = []
         objects = []
 
         if self.tor_sess:
 
-            
             while table == []:
                 try:
-                    page = self.tor_sess.get(url)#requests.get(url)
-                    soup=BeautifulSoup(page.text,"html.parser")
-                    table = soup.find("div",class_="a-section a-spacing-none mojo-performance-summary-table")
+                    page = self.tor_sess.get(url)  # requests.get(url)
+                    soup = BeautifulSoup(page.text, "html.parser")
+                    table = soup.find(
+                        "div",
+                        class_="a-section a-spacing-none mojo-performance-summary-table",
+                    )
 
                     if table:
                         objects = table.find_all("div")
                     else:
                         raise requests.exceptions.ConnectionError
                 except requests.exceptions.ConnectionError:
-                    print('renewing connection')
+                    print("renewing connection")
                     renew_connection()
                     self.tor_sess = get_tor_session()
                     sleep(5)
 
         else:
             page = requests.get(url)
-            soup=BeautifulSoup(page.text,"html.parser")
-            table = soup.find("div",class_="a-section a-spacing-none mojo-performance-summary-table")
-            
+            soup = BeautifulSoup(page.text, "html.parser")
+            table = soup.find(
+                "div", class_="a-section a-spacing-none mojo-performance-summary-table"
+            )
+
             if table:
                 objects = table.find_all("div")
                 print(imdb_id)
 
         return objects
-    
-    def __get_single_movie_row(self,objects):
+
+    def __get_single_movie_row(self, objects):
         row = {}
         for o in objects:
-            col = re.findall("[a-zA-Z]+",o.find('span',class_="a-size-small").text.strip())[0]
-            data = o.find('span',class_="a-size-medium a-text-bold").text.strip()
-            
-            if (data =='-')|(data=='–'):
+            col = re.findall(
+                "[a-zA-Z]+", o.find("span", class_="a-size-small").text.strip()
+            )[0]
+            data = o.find("span", class_="a-size-medium a-text-bold").text.strip()
+
+            if (data == "-") | (data == "–"):
                 data = np.nan
             else:
-                data = float(Decimal(re.sub(r'[^\d.]', '', data)))
-                
+                data = float(Decimal(re.sub(r"[^\d.]", "", data)))
+
             row[col] = data
         return row
 
-    
     def extract_movies(self):
         tot_movies = len(self.imdb_id_list)
         c = 1
         for imdb_id in self.imdb_id_list:
             objects = self.__get_movies_objects(imdb_id)
             row = self.__get_single_movie_row(objects)
-            row['imdb_id'] = imdb_id
+            row["imdb_id"] = imdb_id
             self.movies.append(row)
-            
 
-            checkpoint = round(tot_movies/10)-1
-            completed_pct = c*100/tot_movies
+            checkpoint = round(tot_movies / 10) - 1
+            completed_pct = c * 100 / tot_movies
 
-            if checkpoint>0:
-                if (c%checkpoint==0):
-                    print(f'{completed_pct}% done!')
+            if checkpoint > 0:
+                if c % checkpoint == 0:
+                    print(f"{completed_pct}% done!")
 
-                    self.to_df().to_csv(f'data/raw/box_office_mojo_data_chkp_{c}_{imdb_id}.csv',index=False)
-                    self.movies = [] # reset otherwise would generate duplicates
+                    self.to_df().to_csv(
+                        f"data/raw/box_office_mojo_data_chkp_{c}_{imdb_id}.csv",
+                        index=False,
+                    )
+                    self.movies = []  # reset otherwise would generate duplicates
 
-            if (c%100==0)&(self.tor_sess is not None):
+            if (c % 100 == 0) & (self.tor_sess is not None):
                 renew_connection()
                 self.tor_sess = get_tor_session()
-                #print(self.tor_sess.get("http://icanhazip.com").text)
+                # print(self.tor_sess.get("http://icanhazip.com").text)
                 print("getting new ip")
-            c+=1
+            c += 1
         return self.to_df()
-    
+
     def to_df(self):
         return pd.DataFrame(self.movies)
+
 
 from time import sleep
 import multiprocess as mp
@@ -120,20 +131,21 @@ from glob import glob
 
 if __name__ == "__main__":
 
-    imdb_ids_list = pd.read_csv('data/processed/filtered_id_list.csv')['imdb_id'].values
+    imdb_ids_list = pd.read_csv("data/processed/filtered_id_list.csv")["imdb_id"].values
 
-    
-    scraped_files =  glob('data/raw/*chkp*.csv')
+    scraped_files = glob("data/raw/*chkp*.csv")
 
     if scraped_files:
-        imdb_ids_scraped = pd.concat([pd.read_csv(i,usecols=['imdb_id']) \
-            for i in glob('data/raw/*chkp*.csv')],ignore_index=True).values.flatten()
+        imdb_ids_scraped = pd.concat(
+            [pd.read_csv(i, usecols=["imdb_id"]) for i in glob("data/raw/*chkp*.csv")],
+            ignore_index=True,
+        ).values.flatten()
         imdb_ids_list = list(set(imdb_ids_list) - set(imdb_ids_scraped))
 
-    print(f'scraping {len(imdb_ids_list)} movies')
-    
-    def extract(imdb_ids,use_tor = True):
-        
+    print(f"scraping {len(imdb_ids_list)} movies")
+
+    def extract(imdb_ids, use_tor=True):
+
         tor_sess = None
 
         if use_tor:
@@ -142,32 +154,34 @@ if __name__ == "__main__":
             sleep(5)
             tor_sess = get_tor_session()
 
-        bom2=BoxOfficeMojoScraper(imdb_ids,tor_sess)
+        bom2 = BoxOfficeMojoScraper(imdb_ids, tor_sess)
         return bom2.extract_movies()
 
     n_jobs = mp.cpu_count()
 
-    batches = list(range(1,len(imdb_ids_list),len(imdb_ids_list)//n_jobs))
+    batches = list(range(1, len(imdb_ids_list), len(imdb_ids_list) // n_jobs))
     imdb_ids_batches = []
-    for i,j in zip(batches[:-1],batches[1:]):
+    for i, j in zip(batches[:-1], batches[1:]):
         imdb_ids_batches.append(list(imdb_ids_list[i:j]))
-    imdb_ids_batches.append(list(imdb_ids_list[batches[-1]:]))
+    imdb_ids_batches.append(list(imdb_ids_list[batches[-1] :]))
 
     with mp.Pool(n_jobs) as p:
-        data = p.map(extract,imdb_ids_batches)
-    
+        data = p.map(extract, imdb_ids_batches)
 
-    #box_office_mojo_data_v2 = pd.concat(box_office_mojo_data_v2,ignore_index=True)
+    # box_office_mojo_data_v2 = pd.concat(box_office_mojo_data_v2,ignore_index=True)
 
-    #Save dataframe
+    # Save dataframe
 
-    box_office_mojo_data_v2 = pd.concat([pd.read_csv(i) \
-        for i in glob('data/raw/*chkp*.csv')],ignore_index=True)
-    
-    box_office_mojo_data_v2 = box_office_mojo_data_v2\
-            .drop_duplicates()\
-            .dropna(subset=['Worldwide'])
+    box_office_mojo_data_v2 = pd.concat(
+        [pd.read_csv(i) for i in glob("data/raw/*chkp*.csv")], ignore_index=True
+    )
 
-    box_office_mojo_data_v2.reset_index(drop=True,inplace=True)
+    box_office_mojo_data_v2 = box_office_mojo_data_v2.drop_duplicates().dropna(
+        subset=["Worldwide"]
+    )
 
-    box_office_mojo_data_v2.to_csv('data/processed/box_office_mojo_data_v2.csv',index=False)
+    box_office_mojo_data_v2.reset_index(drop=True, inplace=True)
+
+    box_office_mojo_data_v2.to_csv(
+        "data/processed/box_office_mojo_data_v2.csv", index=False
+    )
--- src/data/imdb_api_data.py
+++ src/data/imdb_api_data.py
@@ -5,146 +5,167 @@ from imdb import Cinemagoer
 import numpy as np
 import os
 
-def extract_from_list_col(dataframe,col,max_items=4,normalize=True):
-    return dataframe[col].apply(lambda x: extract_from_list(x,
-                                                            max_items=max_items,
-                                                            normalize=normalize))
 
-def extract_from_list(list_,max_items=4,normalize=True):
+def extract_from_list_col(dataframe, col, max_items=4, normalize=True):
+    return dataframe[col].apply(
+        lambda x: extract_from_list(x, max_items=max_items, normalize=normalize)
+    )
 
-    if isinstance(list_,list):
+
+def extract_from_list(list_, max_items=4, normalize=True):
+
+    if isinstance(list_, list):
         if normalize:
             list_ = [i.lower().strip() for i in list_[:4]]
         else:
             list_ = list_[:4]
     else:
         return np.nan
-        
-    return '|'.join(list_)
 
-COLUMNS_TO_DOWNLOAD = ['director',
-                        'countries',
-                        'country codes',
-                        'language codes',
-                        'languages',
-                        'box office',
-                        'cover url',
-                        'full-size cover url',
-                        'production companies']
+    return "|".join(list_)
+
+
+COLUMNS_TO_DOWNLOAD = [
+    "director",
+    "countries",
+    "country codes",
+    "language codes",
+    "languages",
+    "box office",
+    "cover url",
+    "full-size cover url",
+    "production companies",
+]
+
 
 class IMDBApiDataLoader:
-
-    def __init__(self,
-                 input_dir = 'data/raw',
-                 output_dir= 'data/processed',
-                 columns_to_download = COLUMNS_TO_DOWNLOAD):
+    def __init__(
+        self,
+        input_dir="data/raw",
+        output_dir="data/processed",
+        columns_to_download=COLUMNS_TO_DOWNLOAD,
+    ):
 
         self.columns_to_download = columns_to_download
         self.input_dir = input_dir
         self.output_dir = output_dir
 
-    def download_from_api(self,ids_to_download,movies_per_file = 100):
+    def download_from_api(self, ids_to_download, movies_per_file=100):
         # Get Data From API
         loaded_ids = []
         cg = Cinemagoer()
 
-        #Continue where we left off
-        files_names = glob(os.path.join(self.input_dir,'imdb_api_chkp_*.pickle'))
+        # Continue where we left off
+        files_names = glob(os.path.join(self.input_dir, "imdb_api_chkp_*.pickle"))
         if files_names:
             for i in files_names:
-                with open(i, 'rb') as handle:
-                    b = pickle.load(handle) 
-                loaded_ids.extend([j['imdb_id'] for j in b])
-                
+                with open(i, "rb") as handle:
+                    b = pickle.load(handle)
+                loaded_ids.extend([j["imdb_id"] for j in b])
+
         ids_to_download = list(set(ids_to_download) - set(loaded_ids))
         num_movies = len(ids_to_download)
 
-        print(f'fetching data of {num_movies} movies')
-        c=1
+        print(f"fetching data of {num_movies} movies")
+        c = 1
 
         rows = []
         for imdb_id in ids_to_download:
             obj = cg.get_movie(imdb_id)
-            row = {'imdb_id':imdb_id}
+            row = {"imdb_id": imdb_id}
 
             for col in self.columns_to_download:
 
                 try:
                     row[col] = obj[col]
-                    if col == 'production companies':
-                        row[col] = [i['name'] for i in row['production companies']]
-                    if col == 'director':
-                        row[col] = [i['name'] for i in row['director']]
-                    if col == 'box office':
-                        for k,v in row['box office'].items():
-                            row[k]=v
-                        row.pop('box office')
+                    if col == "production companies":
+                        row[col] = [i["name"] for i in row["production companies"]]
+                    if col == "director":
+                        row[col] = [i["name"] for i in row["director"]]
+                    if col == "box office":
+                        for k, v in row["box office"].items():
+                            row[k] = v
+                        row.pop("box office")
                 except KeyError:
                     row[col] = np.nan
-            
+
             rows.append(row)
 
-            if c%movies_per_file==0:
-                print(round(c*100/num_movies,2))
-                with open(os.path.join(self.input_dir,f'imdb_api_chkp_{c}_{imdb_id}.pickle'), 'wb') as handle:
+            if c % movies_per_file == 0:
+                print(round(c * 100 / num_movies, 2))
+                with open(
+                    os.path.join(self.input_dir, f"imdb_api_chkp_{c}_{imdb_id}.pickle"),
+                    "wb",
+                ) as handle:
                     pickle.dump(rows, handle, protocol=pickle.HIGHEST_PROTOCOL)
                 rows = []
-            c+=1
+            c += 1
 
     def to_df(self):
         # Generate dataframe from downloaded data
         data = []
-        files_names = glob(os.path.join(self.input_dir,'imdb_api_chkp_*.pickle'))
+        files_names = glob(os.path.join(self.input_dir, "imdb_api_chkp_*.pickle"))
         if files_names:
             for i in files_names:
-                with open(i, 'rb') as handle:
-                    b = pickle.load(handle) 
+                with open(i, "rb") as handle:
+                    b = pickle.load(handle)
                 data.extend(b)
         data = pd.DataFrame(data)
         return data
 
     # Clean a bit
-    def clean(self,data):
-        data['Opening Weekend'] = data[[i for i in data.columns if 'Opening Weekend' in i]].apply(lambda x: x.dropna().sum(), axis=1)
+    def clean(self, data):
+        data["Opening Weekend"] = data[
+            [i for i in data.columns if "Opening Weekend" in i]
+        ].apply(lambda x: x.dropna().sum(), axis=1)
 
-        data = data[['imdb_id',
-                    'director', 
-                    'countries', 
-                    'country codes', 
-                    'language codes',
-                    'languages', 
-                    'Budget', 
-                    'cover url', 
-                    'full-size cover url',
-                    'production companies',
-                    'Opening Weekend']]
+        data = data[
+            [
+                "imdb_id",
+                "director",
+                "countries",
+                "country codes",
+                "language codes",
+                "languages",
+                "Budget",
+                "cover url",
+                "full-size cover url",
+                "production companies",
+                "Opening Weekend",
+            ]
+        ]
 
-        list_cols = ['director',
-                    'countries',
-                    'country codes',
-                    'language codes',
-                    'languages',
-                    'production companies']
+        list_cols = [
+            "director",
+            "countries",
+            "country codes",
+            "language codes",
+            "languages",
+            "production companies",
+        ]
 
         for i in list_cols:
-            data.loc[:,i] = extract_from_list_col(data,i)
-        
+            data.loc[:, i] = extract_from_list_col(data, i)
+
         return data
 
-    def to_csv(self,data):
+    def to_csv(self, data):
         # Save
-        data.to_csv(os.path.join(self.output_dir,'imdb_api_data.csv'),index=False)
-    
-    def run_all(self,ids_to_download,movies_per_file = 100):
-        self.download_from_api(ids_to_download,movies_per_file)
-        data=self.to_df()
+        data.to_csv(os.path.join(self.output_dir, "imdb_api_data.csv"), index=False)
+
+    def run_all(self, ids_to_download, movies_per_file=100):
+        self.download_from_api(ids_to_download, movies_per_file)
+        data = self.to_df()
         data = self.clean(data)
         self.to_csv(data)
         return data
 
+
 if __name__ == "__main__":
 
-    ids_to_download = pd.read_csv('data/processed/filtered_id_list.csv')['imdb_id'].unique()
+    ids_to_download = pd.read_csv("data/processed/filtered_id_list.csv")[
+        "imdb_id"
+    ].unique()
 
     idl = IMDBApiDataLoader()
     idl.run_all(ids_to_download)
